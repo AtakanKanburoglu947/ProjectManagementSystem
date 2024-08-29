6ea5d2ed8623f342ca55f0f886371fbd59d12de8
Atakan Kanburoglu <88630930+AtakanKanburoglu947@users.noreply.github.com>
2024-08-29 14:53:44 +0300

    IService ve Service eklendi

---
+++ ProjectManagementSystemService/IService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagementSystemService
+{
+    public interface IService<T,Dto> where T : class where Dto : class
+    {
+         Task Add(Dto dto);
+         Task Update(Dto dto);
+         Task Remove(Dto dto);
+         Task<List<T>> GetAll();
+         Task<T> Get(int id);
+         Task<T> Get(Expression<Func<T,bool>> expression);    
+         Task<List<T>> Filter(Expression<Func<T,bool>> expression);
+
+    }
+}
---
+++ ProjectManagementSystemService/Service.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using ProjectManagementSystemRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagementSystemService
+{
+    public class Service<T, Dto> : IService<T, Dto> where T : class where Dto : class
+    {
+        private readonly IMapper _mapper;
+        private readonly AppDbContext _appDbContext;
+        private readonly DbSet<T> _dbSet;   
+        public Service(IMapper mapper, AppDbContext appDbContext)
+        {
+            _mapper = mapper;
+            _appDbContext = appDbContext;
+            _dbSet = _appDbContext.Set<T>();
+        }
+        public async Task Add(Dto dto)
+        {
+            _dbSet.Add(_mapper.Map<T>(dto));
+            await _appDbContext.SaveChangesAsync();
+
+        }
+
+        public async Task Remove(Dto dto)
+        {
+            _dbSet.Remove(_mapper.Map<T>(dto));
+            await _appDbContext.SaveChangesAsync();
+        }
+
+        public async Task<T> Get(int id)
+        {
+            return await _dbSet.FindAsync(id); 
+        }
+
+        public async Task<List<T>> GetAll()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task Update(Dto dto)
+        {
+            _dbSet.Update(_mapper.Map<T>(dto));
+            await _appDbContext.SaveChangesAsync();
+        }
+
+        public async Task<List<T>> Filter(Expression<Func<T, bool>> expression)
+        {
+            return await _dbSet.Where(expression).ToListAsync();    
+        }
+
+        public async Task<T> Get(Expression<Func<T, bool>> expression)
+        {
+            return await _dbSet.FirstOrDefaultAsync(expression);
+        }
+    }
+}
