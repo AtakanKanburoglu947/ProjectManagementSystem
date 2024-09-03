7c572d1f6b386fc3b6c0e44d7a4c6732cabab797
Atakan Kanburoglu <88630930+AtakanKanburoglu947@users.noreply.github.com>
2024-09-03 10:22:41 +0300

    ManagerController, ManagerDto ve ManagerUpdateDto eklendi

---
+++ ProjectManagementSystemAPI/Controllers/ManagerController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProjectManagementSystemCore.Dtos;
+using ProjectManagementSystemCore.Models;
+using ProjectManagementSystemService;
+
+namespace ProjectManagementSystemAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ManagerController : ControllerBase
+    {
+        private readonly IService<Manager, ManagerDto, ManagerUpdateDto> _managerService;
+        public ManagerController(IService<Manager,ManagerDto,ManagerUpdateDto> managerService)
+        {
+            _managerService = managerService;
+        }
+
+    }
+}
---
+++ ProjectManagementSystemCore/Dtos/ManagerDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagementSystemCore.Dtos
+{
+    public class ManagerDto
+    {
+        public Guid UserIdentityId { get; set; }
+        public int RoleId { get; set; }
+
+    }
+}
