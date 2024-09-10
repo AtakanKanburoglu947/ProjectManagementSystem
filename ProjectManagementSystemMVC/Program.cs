6bacce47ae256d897160b4e84c6e47d1bb15b41b
Atakan Kanburoglu <88630930+AtakanKanburoglu947@users.noreply.github.com>
2024-09-10 16:46:05 +0300

    Program.cs güncellendi

--- ProjectManagementSystemMVC/Program.cs
+++ ProjectManagementSystemMVC/Program.cs
@@ -10,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 builder.Services.AddAutoMapper(typeof(DtoMapper).Assembly);
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
